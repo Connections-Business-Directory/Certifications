b6c9c50f7bd11dec0c996a873c1a730304067447
Steven A. Zahm <shazahm1@hotmail.com>
2021-05-04 09:30:24 -0400

    Remove use of `create_function()`.

--- connections-certifications.php
+++ connections-certifications.php
@@ -738,10 +738,9 @@ HEREDOC;
 
 				add_action(
 					'admin_notices',
-					 create_function(
-						 '',
-						'echo \'<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Certifications.</p></div>\';'
-						)
+					function() {
+						echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Certifications.</p></div>';
+					}
 				);
 
 				return FALSE;
