4c2741065fefd5d35ce4643a244ea18707a10bb0
Steven A. Zahm <shazahm1@hotmail.com>
2021-05-04 09:31:16 -0400

    Correct code alignment.

--- connections-certifications.php
+++ connections-certifications.php
@@ -730,21 +730,21 @@ HEREDOC;
 	 */
 	function Connections_Certifications() {
 
-			if ( class_exists('connectionsLoad') ) {
+		if ( class_exists( 'connectionsLoad' ) ) {
 
-					return new Connections_Certifications();
+			return new Connections_Certifications();
 
-			} else {
+		} else {
 
-				add_action(
-					'admin_notices',
-					function() {
-						echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Certifications.</p></div>';
-					}
-				);
+			add_action(
+				'admin_notices',
+				function() {
+					echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Certifications.</p></div>';
+				}
+			);
 
-				return FALSE;
-			}
+			return false;
+		}
 	}
 
 	/**
